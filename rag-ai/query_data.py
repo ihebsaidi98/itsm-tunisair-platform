10dcb1eb7967de7814e02b8ee9a13ac84a67eb82
ihebsaidi98 <saidi.iheb@esprit.tn>
2026-09-16 12:31:07 +0100

    Update

--- rag-ai/query_data.py
+++ rag-ai/query_data.py
@@ -1,15 +1,11 @@
 import argparse
-from langchain.vectorstores.chroma import Chroma
-from langchain.prompts import ChatPromptTemplate
-from langchain_community.llms.ollama import Ollama
-
-from get_embedding_function import get_embedding_function
-
-
 import os
 
-from groq import Groq
+from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
 
+from get_embedding_function import get_embedding_function
+from groq import Groq
 
 CHROMA_PATH = "chroma"
 
@@ -22,16 +18,8 @@ Answer the question based only on the following context:
 
 Answer the question based on the above context: {question}
 """
-import os
+
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
-
-client = Groq(
-    api_key=GROQ_API_KEY
-)
-
-
-
-
 
 
 def main():
@@ -45,7 +33,6 @@ def main():
 
 def query_rag(query_text: str):
     # Prepare the DB.
-
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
@@ -53,14 +40,12 @@ def query_rag(query_text: str):
     results = db.similarity_search_with_score(query_text, k=5)
 
     context_text = "\n\n---\n\n".join([
-    doc.page_content.replace("\n", " ").strip() for doc, _ in results
+        doc.page_content.replace("\n", " ").strip() for doc, _ in results
     ])
 
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
 
-
- 
     try:
         chat_completion = client.chat.completions.create(
             messages=[
